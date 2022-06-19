14bf792860427c548ffa5ec107d8623cfc2b2030
yasir <hellopleasantcodes@gmail.com>
2022-06-19 08:41:11 +0100

    created the product list page using dynmic card rendering from the material ui

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,49 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
 import Layouts from '../components/Layouts';
-import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layouts>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={product.name}
+                    // height="140"
+                    image={product.image}
+                    title={product.name}
+                  />
+
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+
+                <CardActions>
+                  <Typography>₦{product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layouts>
   );
