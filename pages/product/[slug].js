28a9d05b9629807c575d79c02637ef1dc87daeee
Yasir Gaji <gajiyasir@gmail.com>
2022-07-03 15:37:59 +0100

    did dynamic image rendering

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,11 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Link } from '@material-ui/core';
+import { Grid, Link } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import data from '../../utils/data';
 import Layout from '../../components/Layouts';
 import useStyles from '../../utils/styles';
+import Image from 'next/image';
 
 export default function ProductScreen() {
   const classes = useStyles();
@@ -22,6 +23,18 @@ export default function ProductScreen() {
           <Link>Back to products</Link>
         </NextLink>
       </div>
+
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
