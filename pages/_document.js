8a61fcd23e5a43454eb57a3c727160babef7db81
Yasir Gaji <gajiyasir@gmail.com>
2022-07-07 08:40:49 +0100

    changed the fonts

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <Link
+          <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
